b6bf7825792cafa6056e86963f744e606fcbf8a3
Cory <cory.davis@treasury.govt.nz>
2023-06-19 08:44:01 +1200

    add disclaimer

--- main_panel.py
+++ main_panel.py
@@ -48,16 +48,17 @@ do_plot(fig, pop_selector.value, measure_selector.value)
 
 mpl = pn.pane.Matplotlib(fig).servable(target='plot-area')
 
+text = pn.pane.Markdown(
+    "These results are not official statistics. They have been created for research purposes from the "
+    "Integrated Data Infrastructure (IDI) which is carefully managed by Stats NZ. For more information "
+    "about the IDI please visit https://www.stats.govt.nz/integrated-data/. The results are based in part "
+    "on tax data supplied by Inland Revenue to Stats NZ under the Tax Administration Act 1994 for "
+    "statistical purposes. Any discussion of data limitations or weaknesses is in the context of using "
+    "the IDI for statistical purposes, and is not related to the data’s ability to support Inland Revenue’s "
+    "core operational requirements.").servable(target='text-area')
 
 def update(event):
     do_plot(fig, pop_selector.value, measure_selector.value)
     mpl.param.trigger('object')
-    
-    
-    
-    #table(subset_data)
 
 go_button.on_click(update)
-
-#ele_proxy = create_proxy(update)
-#income_selector.addEventListener("change", ele_proxy)
