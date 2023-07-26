815775a26329e488adf10df1ed3c7af38e1bf6bd
Cory <cory.davis@treasury.govt.nz>
2023-07-26 15:02:14 +1200

    emtr plot looking pretty good now

--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+
 
 import emtr
 
@@ -24,9 +26,53 @@ def fig_table_data(
     output = pd.concat([sq_output, reform_output], axis=0)
     output['scenario'] = ['SQ']*len(sq_output) + ['Reform']*len(reform_output)
 
-    fig = do_plotly(output)
+    fig = rate_plot(output, 'emtr')
     output.to_csv('output.csv', index=False)
     return fig, output
+
+
+def rate_plot(output, var_name):
+    y_label = {
+        'emtr': 'Effective marginal tax rate',
+        'replacement_rate': 'Replacement rate',
+        'participaction_rate': 'Participation tax rate'
+    }[var_name]
+
+    # subset to the relevant columns
+    plot_data = output[['gross_wage1_annual', 'hours1', var_name, 'scenario']].copy()
+
+    # clip the values to the range 0-1.1
+    plot_data[var_name] = plot_data[var_name].clip(lower=0, upper=1.1)
+
+    fig = px.line(
+        plot_data, x='gross_wage1_annual', y=var_name, color='scenario', 
+        labels={"gross_wage1_annual": "Annual gross wage income ($)", var_name: y_label},
+        line_dash="scenario",
+        color_discrete_sequence=["#56B4E9", "#E69F00"], 
+        template="plotly_white")
+    
+    fig.update_traces(hovertemplate=None)
+
+    fig.add_trace(
+        go.Scatter(
+            x=plot_data['hours1'], y=[0]*len(plot_data), line=dict(width=0),
+            xaxis="x2", hoverinfo="skip", mode="lines", showlegend=False))
+    
+    fig.update_layout(
+        xaxis2={
+            'overlaying': "x", 'nticks': 10, 'side': "top", 'title': "Hours/week", 
+            'automargin': True, 'showline': True},
+        xaxis={
+            'title': "Annual gross wage income ($)", 'tickprefix': "$",
+            'automargin': True, 'showline': True, 'mirror': True},
+        
+        yaxis={
+            'title': y_label, 'tickformat': ".0%", 
+            'automargin': True, 'showline': True, 'mirror': True},
+        legend={'x': 100, 'y': 0.5},
+        hovermode="x")
+        
+    return fig
 
 
 def do_plotly(output):
