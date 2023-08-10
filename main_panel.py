5791ac8a978d3e8a2ecf2613ac5eadbb0f99682c
Cory <cory.davis@treasury.govt.nz>
2023-08-10 18:02:34 +1200

    bugfix, README and instructions

--- main_panel.py
+++ main_panel.py
@@ -3,11 +3,10 @@ import panel as pn
 from helpers import *
 
 pn.extension(sizing_mode="stretch_width")
-pn.extension('plotly')
 
 title = pn.Column(
     pn.Row(
-        pn.pane.Markdown('# Income Explorer Prototype (UNDER CONSTRUCTION !)', width=600),
+        pn.pane.Markdown('# Income Explorer Prototype', width=600),
         pn.pane.Markdown('*Best viewed full screen*', align = ('end', 'end'))),
     # add a horizontal line
     pn.layout.Divider()).servable(target='title')
@@ -35,7 +34,7 @@ accom_cost_input = pn.widgets.FloatInput(name = 'Weekly Accom.\n Cost', value = 
 as_area_input = pn.widgets.Select(name = 'AS Area', options = [1, 2, 3, 4], value = 1)
 accom_type_input = pn.widgets.Select(
     name = 'Accom. Type', options = ['Rent', 'Mortgage'], value = 'Rent')
-child_age_input = pn.widgets.TextInput(name = 'children_ages', value = "0, 5, 14")
+child_age_input = pn.widgets.TextInput(name = 'Child Ages', value = "0, 5, 14")
 
 #Add the "Partnered" toggle, which upon clicking, adds the controls for the second wage
 partner_toggle = pn.widgets.Toggle(
@@ -44,8 +43,6 @@ partner_hrly_wage_input = pn.widgets.FloatInput(
     name = 'Partner Hourly Wage', value = 20, disabled = True, width=120)
 partner_hours_worked_input = pn.widgets.IntInput(
     name = 'Partner Hours Worked', value = 0, disabled = True, width=120)
-#partner_row = pn.Row(
-#    partner_hrly_wage_input, partner_hours_worked_input, width = 300)
 
 # watch the partner toggle to enable/disable the partner controls
 def update_partner_controls(event):
@@ -62,7 +59,7 @@ partner_toggle.param.watch(update_partner_controls, 'value')
 
 
 go_button = pn.widgets.Button(
-    name='Calculate !', button_type='success', width=50, align=('start', 'center'))
+    name='Calculate !', button_type='success', width=100, height=60, align=('start', 'center'))
 
 data_download = pn.widgets.FileDownload(
     'output.csv', label='Download output.csv', button_type='primary', 
@@ -70,7 +67,8 @@ data_download = pn.widgets.FileDownload(
 
 widget_box = pn.WidgetBox(
     pn.pane.Markdown('### Policy parameters'),
-    pn.Row(pn.pane.Markdown('**Name**', width=100), pn.pane.Markdown('**Built-in or uploaded parameter file**')),
+    pn.Row(pn.pane.Markdown('**Name**', width=100), 
+           pn.pane.Markdown('**Built-in (BEFU23) or uploaded parameter file**')),
     policy_controls[0].row,
     policy_controls[1].row,
     policy_controls[2].row,
@@ -82,7 +80,8 @@ widget_box = pn.WidgetBox(
     pn.Row(partner_toggle, partner_hrly_wage_input, partner_hours_worked_input),
     pn.layout.Divider(),
     pn.Row(income_choice_input, wep_scaling_input),
-    go_button, data_download,
+    go_button,
+    data_download,
     width = 450).servable(target='widget_box')
 
 # ------------------------------------------------------------------------------------
@@ -131,10 +130,12 @@ pn.Tabs(
     emtr_tab, comp_tab, instructions, 
     width = 1500, height=2000, active=0).servable(target='tabs')
 
+
 #-------------------------------------------------------------------------------------
 
 def update(event):
     """Update the plot and table when the Go button is clicked"""
+
     child_ages = string_to_list_of_integers(child_age_input.value)
 
     params = {pc.name_input.value: pc.params for pc in policy_controls if pc.params is not None}
@@ -145,11 +146,9 @@ def update(event):
         accom_cost_input.value, accom_type_input.value, as_area_input.value, 
         max_hours_input.value, WEPScaling[wep_scaling_input.value],
         IncomeChoice[income_choice_input.value])
-    
     for key in rate_figs:
         rate_panes[key].object=rate_figs[key].to_html()
 
     update_comp_tab(comp_figs)
-
-
+ 
 go_button.on_click(update)
--- test/test_emtr.py
+++ test/test_emtr.py
@@ -45,7 +45,7 @@ class TestEmtr(EMTRTest):
             parameters = yaml.safe_load(f)
         output = emtr(
             parameters, partnered = False, wage1_hourly = 18.50,
-            children_ages = [0, 1, 10], gross_wage2 = 0, hours2 = 0,
+            children_ages = [0, 1, 10], wage2_hourly = 0, hours2 = 0,
             as_accommodation_costs = 600, as_accommodation_rent = True, as_area = 1)
         output.to_csv('test/output/emtr_output_1.csv', index=False)
         self.compare_with_ref_file(output, 'test/ref/emtr_output_1.csv')
@@ -56,7 +56,7 @@ class TestEmtr(EMTRTest):
             parameters = yaml.safe_load(f)
         output = emtr(
             parameters, partnered = True, wage1_hourly = 18.50,
-            children_ages = [2, 15], gross_wage2 = 0, hours2 = 0,
+            children_ages = [2, 15], wage2_hourly = 0, hours2 = 0,
             as_accommodation_costs = 800, as_accommodation_rent = False, as_area = 2)
         output.to_csv('test/output/emtr_output_2.csv', index=False)
         self.compare_with_ref_file(output, 'test/ref/emtr_output_2.csv')
@@ -67,7 +67,7 @@ class TestEmtr(EMTRTest):
             parameters = yaml.safe_load(f)
         output = emtr(
             parameters, partnered = True, wage1_hourly = 18.50,
-            children_ages = [9], gross_wage2=185, hours2=10,
+            children_ages = [9], wage2_hourly = 18.5, hours2=10,
             as_accommodation_costs = 600, as_accommodation_rent = True, 
             as_area = 3)
         output.to_csv('test/output/emtr_output_3.csv', index=False)
@@ -79,7 +79,7 @@ class TestEmtr(EMTRTest):
             parameters = yaml.safe_load(f)
         output = emtr(
             parameters, partnered = True, wage1_hourly = 18.50,
-            children_ages = [], gross_wage2 = 370,
+            children_ages = [], wage2_hourly = 18.50, hours2=20,
             as_accommodation_costs = 500, as_accommodation_rent = False, 
             as_area = 4)
         output.to_csv('test/output/emtr_output_4.csv', index=False)
@@ -91,7 +91,7 @@ class TestEmtr(EMTRTest):
             parameters = yaml.safe_load(f)
         output = emtr(
             parameters, partnered = True, wage1_hourly = 18.50,
-            children_ages = [], gross_wage2 = 0,
+            children_ages = [], wage2_hourly=0, hours2=0,
             as_accommodation_costs = 800, as_accommodation_rent = False, 
             as_area = 1)
         output.to_csv('test/output/emtr_output_5.csv', index=False)
@@ -103,7 +103,7 @@ class TestEmtr(EMTRTest):
             parameters = yaml.safe_load(f)
         output = emtr(
             parameters, partnered = False, wage1_hourly = 18.50,
-            children_ages = [], gross_wage2 = 0,
+            children_ages = [], wage2_hourly=0, hours2=0,
             as_accommodation_costs = 0, as_accommodation_rent = True, 
             as_area = 2)
         output.to_csv('test/output/emtr_output_6.csv', index=False)
